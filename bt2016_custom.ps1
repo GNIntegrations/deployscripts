156c77a8b6e0c142cb345288cf46fdcd0200ac6a
GNIntegrations <geoffl@magenic.com>
2017-07-11 10:53:33 -0400

    Install line
    Added install line

--- bt2016_custom.ps1
+++ bt2016_custom.ps1
@@ -1,1 +1,2 @@
 Invoke-WebRequest https://www.microsoft.com/en-us/download/confirmation.aspx?id=53016 -outfile c:\temp\azurelogicapps.msi
+Start-Process msiexec.exe -Wait -ArgumentList '/I C:\temp\azurelogicapps.msi /quiet'
